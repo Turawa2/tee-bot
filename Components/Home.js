c42c3bffdd3ab79463c20ad1214de33030a9572d
Turawa <hamzamusaturawa@gmail.com>
2024-02-23 17:32:59 +0100

    tailwind commit

---
+++ Components/Home.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Welcome() {
+  return (
+    <View style={{paddingTop: 45}}  className="bg-red-200">
+      <Text className=" text-3xl text-center ">Home</Text>
+    </View>
+  )
+}
